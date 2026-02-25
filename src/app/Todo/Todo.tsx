e14fa3091bb84d6e9e23a5249d00389f2f1d8f5f
Zongming Li <lzm041201@gmail.com>
2026-02-25 10:54:42 -0500

    Style todo items and show completed status

--- src/app/Todo/Todo.tsx
+++ src/app/Todo/Todo.tsx
@@ -6,10 +6,18 @@ export type TodoItem = {
   completed: boolean;
 };
 
-export const Todo = ({ id, text, completed }: TodoItem) => {
+export const Todo = ({ text, completed }: TodoItem) => {
   return (
-    <div className="flex flex-row gap-[32px]">
-      <div>{text}</div>
+    <div
+      className={`flex flex-row gap-[32px] bg-blue-50 items-center px-4 py-2 rounded-md ${completed ? "opacity-50" : "opacity-100"}`}
+    >
+      <input
+        type="checkbox"
+        checked={completed}
+        readOnly
+        aria-label={completed ? "Completed todo" : "Incomplete todo"}
+      />
+      <div className={`${completed ? "line-through" : ""}`}>{text}</div>
     </div>
   );
 };
